844cafc087572dea18deec892bd955ce1a98d9c6
Vaillant-S <matvienko.serega2012@gmail.com>
2016-03-16 19:24:40 +0200

    Webpack changes according to eslint specification

--- src/BotFront/webpack.config.js
+++ src/BotFront/webpack.config.js
@@ -1,30 +1,46 @@
 var path = require('path'),
-    webpack = require("webpack"),
-    srcPath = path.join(__dirname, 'app'),
-    buildPath = path.join(__dirname, 'build'),
-    pkg = require('./package.json'),
-    HtmlWebpackPlugin = require('html-webpack-plugin');
+   webpack = require("webpack"),
+   rootPath = __dirname,
+   srcPath = path.join(rootPath, 'app'),
+   buildPath = path.join(rootPath, 'build'),
+   pkg = require('./package.json'),
+   HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: path.join(srcPath, 'main.js'),
-    output: {
-        path: path.join(buildPath),
-        filename: 'bundle.js'
-    },
-    module: {
-        loaders: [{
+   entry: path.join(srcPath, 'main.js'),
+   output: {
+      path: path.join(buildPath),
+      filename: 'bundle.js'
+   },
+   eslint: {
+      configFile: path.join(rootPath, '.eslintrc'),      
+      failOnError: true,
+      emitWarning: true
+   },
+   module: {
+      preLoadres: [
+         {
+            test: /\.js$/,
+            loaders: 'eslint-loader?{rules:{"no-debugger": 2}}',
+            include: srcPath
+         }
+      ],
+      loaders: [
+         {
             test: /index.html/,
             loader: 'file?name=templates/[name]-[hash:6].html'
-        }, {
+         }, 
+         {
             test: /\.html/,
             loader: 'html'
-        }, {
+         }, 
+         {
             test: /\.js$/,
             exclude: ["node_modules"],
             loader: "ng-annotate?add=true!babel?presets[]=es2015"
-        }]
-    },
-    plugins: [
+         }]
+   },
+   plugins: [
       new HtmlWebpackPlugin({
          filename: 'index.html',
          pkg: pkg,
@@ -32,5 +48,5 @@ module.exports = {
       }),
       new webpack.optimize.OccurenceOrderPlugin(),
       new webpack.optimize.DedupePlugin()
-    ]
+   ]
 };

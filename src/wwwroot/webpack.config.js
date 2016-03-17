f3264950ac75f428c9c3a89d51a17add9d333e50
Vaillant-S <matvienko.serega2012@gmail.com>
2016-03-17 16:38:03 +0200

    #17. ESLint configured in webpack and is just waiting for rules to work with.

--- src/wwwroot/webpack.config.js
+++ src/wwwroot/webpack.config.js
@@ -1,55 +1,56 @@
 var path = require('path'),
    webpack = require("webpack"),
    rootPath = __dirname,
-   srcPath = path.join(rootPath, 'app'),
+   appPath = path.join(rootPath, 'app'),
    buildPath = path.join(rootPath, 'build'),
    pkg = require('./package.json'),
    HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-   entry: path.join(srcPath, 'main.js'),
+   entry: path.join(appPath, 'main.js'),
    output: {
       path: path.join(buildPath),
       filename: 'bundle.js'
    },
-   eslint: {
-      configFile: path.join(rootPath, '.eslintrc'),      
-      failOnError: true,
-      emitWarning: true
-   },
    module: {
-      preLoadres: [
+      preLoaders: [
          {
             test: /\.js$/,
-            loaders: 'eslint-loader?{rules:{"no-debugger": 2}}',
-            include: srcPath
+            exclude: /node_modules/,
+            loader: 'eslint-loader'
          }
       ],
       loaders: [
          {
             test: /index.html/,
             loader: 'file?name=templates/[name]-[hash:6].html'
-         }, 
+         },
          {
             test: /\.html/,
             loader: 'html'
-         }, 
+         },
          {
             test: /\.js$/,
             exclude: ["node_modules"],
-            loader: "ng-annotate?add=true!babel?presets[]=es2015"
+            loader: 'ng-annotate?add=true!babel?presets[]=es2015'
          }]
+   },
+   eslint: {
+      configFile: '.eslintrc',
+      emitError: true,
+      emitWarning: true,
+      failOnError: true
    },
    plugins: [
       new HtmlWebpackPlugin({
          filename: 'index.html',
          pkg: pkg,
-         template: path.join(srcPath, 'index.html')
+         template: path.join(appPath, 'index.html')
       }),
       new webpack.optimize.OccurenceOrderPlugin(),
       new webpack.optimize.DedupePlugin()
-    ],
-    devServer: {
+   ],
+   devServer: {
       historyApiFallback: true
-    }
+   }
 };

8487711e32bacb6c45e30596bab3aea050575109
SmolenskiyK <ksmo@design.isd.dp.ua>
2016-03-23 17:23:17 +0200

    add user to storage

--- src/BotData/DumbData/Storage.cs
+++ src/BotData/DumbData/Storage.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BotLibrary.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,27 @@ using System.Threading.Tasks;
 
 namespace BotData.DumbData
 {
-    class Storage
+    public static class Storage
     {
+        public static IList<User> Users = new List<User>()
+        {
+            new User()
+            {
+                Id =1,
+                Login = "admin",
+                Password = "admin",
+                IsActive = true,
+                EditTime = DateTime.Now,
+                ContactInfo = new ContactInfo() { Email = "admin@admin.admin", Id = 1,
+                                                  EditTime = DateTime.Now, IsActive = true, Skype = "admin", PhoneNumbers = new List<string>()},
+                Files = new List<File>(),
+                Location = new Location() { City = new City() {Id = 1, EditTime = DateTime.Now, Country = new Country() {Id = 1,
+                                            EditTime = DateTime.Now, IsActive = true, Name = "Ukraine" }, IsActive = true, Name = "Sicheslav"  },
+                                            EditTime = DateTime.Now, Id = 1, IsActive = true},
+                PersonalInfo = new PersonalInfo() { FirstName = "admin", MiddleName = "admin", LastName = "admin", BirthDate = DateTime.Now, Id = 1,
+                                                    Gender = true, EditTime = DateTime.Now, IsActive = true, Photo = new Photo()},
+                Role = new Role() { Name = "administrator", IsActive = true, EditTime = DateTime.Now, Id = 1, PermissionId = 1}                
+            }               
+        };                
     }
 }

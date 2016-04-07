7412064e0ce23a2fb8cc227a3f728b0308bf347d
SmolenskiyK <ksmo@design.isd.dp.ua>
2016-04-07 14:32:34 +0300

    update validation rules

--- src/wwwroot/app/botConfig.js
+++ src/wwwroot/app/botConfig.js
@@ -13,6 +13,13 @@ import vacancyController from './views/vacancy/vacancy.controller';
 import translationsEn from './translations/translationsEn.json';
 import translationsRu from './translations/translationsRu.json';
 
+import {
+   reduce,
+   isFunction,
+   constant,
+   method
+} from 'lodash';
+
 import context from './context';
 
 export default function _config(
@@ -63,7 +70,35 @@ export default function _config(
 
    LoggerServiceProvider.changeLogLevel(context.logLevel);
    HttpServiceProvider.changeApiUrl(context.serverUrl);
+
+   /*------validation------*/
+   const _true = constant(true);
+   const methods = ['minlength', 'maxlength', 'email', 'number', 'url'];
+   const _origin = reduce(methods, (memo, nameOfMethod) => {
+      memo[nameOfMethod] = _wrap($validationProvider.getExpression(nameOfMethod));
+      return memo;
+   }, {});
+
+   function _wrap(validation) {
+      return isFunction(validation) ? validation : (str) => validation.test(str);
+   }
+   const validationExpression = reduce(methods, (memo, nameOfMethod) => {
+      memo[nameOfMethod] = (value, scope, element, attrs, param) => {
+         let orig = _origin[nameOfMethod];
+         return !value ? _true : orig(value, scope, element, attrs, param);
+      };
+      return memo;
+   }, {});
+   validationExpression.title = function(value) {
+      const minTitleLength = 3;
+      const maxTitleLength = 50;
+      return !!value ? value.length <= maxTitleLength && value.length >= minTitleLength : true;
+   }
+   $validationProvider.showSuccessMessage = false;
    $validationProvider
       .setDefaultMsg({ en: translationsEn.VALIDATION,
-                       ru: translationsRu.VALIDATION}[context.defaultLang || 'en']);
+                       ru: translationsRu.VALIDATION}[context.defaultLang || 'en'])
+      .setExpression(validationExpression)
+      .setValidMethod('blur');
 }
+
--- src/wwwroot/app/views/candidate/candidate.controller.js
+++ src/wwwroot/app/views/candidate/candidate.controller.js
@@ -1,14 +1,22 @@
-export default function CandidateController($scope, CandidateService) {
+export default function CandidateController($scope, CandidateService, $validation) {
    'ngInject';
 
    var vm = $scope;
-   vm.saveCandidate = saveCandidate;
-
-   function saveCandidate() {
-      CandidateService.saveCandidate(vm.candidate).catch(_onError);
-   }
+   vm.submit = _submit;
+   vm.reset = _reset;
 
    function _onError(message) {
       vm.errorMessage = 'Sorry! Some error occurred';
+   }
+
+   function _submit(form) {
+      $validation.validate(form);
+      if ($validation.checkValid(form)) {
+         CandidateService.saveCandidate(vm.candidate).catch(_onError);
+      }
+   }
+
+   function _reset(form) {
+      $validation.reset(form);
    }
 }

2b407a43fa845379c6aa0e2f1295d79a2ccdc2a8
SmolenskiyK <ksmo@design.isd.dp.ua>
2016-04-13 14:07:38 +0300

    2 lines instead one

--- src/wwwroot/app/filters/PhoneFormatFilter.js
+++ src/wwwroot/app/filters/PhoneFormatFilter.js
@@ -1,42 +1,43 @@
-export default function () { return function (tel) {
-   if (!tel) { return ''; }
+export default function () {
+   return function (tel) {
+      if (!tel) { return ''; }
+      var value = tel.toString().trim().replace(/^\+/, '');
 
-   var value = tel.toString().trim().replace(/^\+/, '');
-
-   if (value.match(/[^0-9]/)) {
-      return tel;
-   }
-
-   var country, city, number;
-
-   switch (value.length) {
-      case 10: // +1PPP####### -> C (PPP) ###-##-##
-         country = 1;
-         city = value.slice(0, 3);
-         number = value.slice(3);
-         break;
-
-      case 11: // +CPPP####### -> CCC (PP) ###-##-##
-         country = value[0];
-         city = value.slice(1, 4);
-         number = value.slice(4);
-         break;
-
-      case 12: // +CCCPP####### -> CCC (PP) ###-##-##
-         country = value.slice(0, 3);
-         city = value.slice(3, 5);
-         number = value.slice(5);
-         break;
-
-      default:
+      if (value.match(/[^0-9]/)) {
          return tel;
-   }
+      }
 
-   if (country === 1) {
-      country = '';
-   }
+      var country, city, number;
 
-   number = number.slice(0, 3) + '-' + number.slice(3,5) + '-' + number.slice(5);
+      switch (value.length) {
+         case 10: // +1PPP####### -> C (PPP) ###-##-##
+            country = 1;
+            city = value.slice(0, 3);
+            number = value.slice(3);
+            break;
 
-   return (country + ' (' + city + ') ' + number).trim();
-};}
+         case 11: // +CPPP####### -> CCC (PP) ###-##-##
+            country = value[0];
+            city = value.slice(1, 4);
+            number = value.slice(4);
+            break;
+
+         case 12: // +CCCPP####### -> CCC (PP) ###-##-##
+            country = value.slice(0, 3);
+            city = value.slice(3, 5);
+            number = value.slice(5);
+            break;
+
+         default:
+            return tel;
+      }
+
+      if (country === 1) {
+         country = '';
+      }
+
+      number = number.slice(0, 3) + '-' + number.slice(3,5) + '-' + number.slice(5);
+
+      return (country + ' (' + city + ') ' + number).trim();
+   };
+}

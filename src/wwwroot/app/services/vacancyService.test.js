c8cdb3cf176c535780f824fd02238b94f73c0571
Vaillant-S <matvienko.serega2012@gmail.com>
2016-04-14 15:51:38 +0300

    VacancyService tests on calls

--- src/wwwroot/app/services/vacancyService.test.js
+++ src/wwwroot/app/services/vacancyService.test.js
@@ -1,13 +1,13 @@
 'use strict';
 
-import vacancyService from '../services/vacancyService';
+import vacancyService from './vacancyService';
 
 describe('VacancyService tests for', () => {
    let service = null;
-   let mock    = {
-      get   : jasmine.createSpy(),
-      post  : jasmine.createSpy(),
-      put   : jasmine.createSpy(),
+   let mock = {
+      get: jasmine.createSpy(),
+      post: jasmine.createSpy(),
+      put: jasmine.createSpy(),
       remove: jasmine.createSpy()
    };
 
@@ -19,11 +19,60 @@ describe('VacancyService tests for', () => {
       });
    });
 
-   it('getVacancies not to be undefined', inject((VacancyService) => {
+   it('getVacancies not to be undefined or null', inject((VacancyService) => {
       expect(VacancyService.getVacancies).not.toBeUndefined();
+      expect(VacancyService.getVacancies).not.toBeNull();
    }));
 
-   it('getVacancies not to be null', inject((VacancyService) => {
-      expect(VacancyService.getVacancies).not.toBeNull();
+   it('getVacancy not to be null', inject((VacancyService) => {
+      expect(VacancyService.getVacancy).not.toBeUndefined();
+      expect(VacancyService.getVacancy).not.toBeNull();
+   }));
+
+   it('saveVacancy not to be null', inject((VacancyService) => {
+      expect(VacancyService.saveVacancy).not.toBeUndefined();
+      expect(VacancyService.saveVacancy).not.toBeNull();
+   }));
+
+   it('deleteVacancy not to be null', inject((VacancyService) => {
+      expect(VacancyService.deleteVacancy).not.toBeUndefined();
+      expect(VacancyService.deleteVacancy).not.toBeNull();
+   }));
+
+   it('getVacancies call test', inject((VacancyService) => {
+      let vacancies = VacancyService.getVacancies();
+      expect(mock.get).toHaveBeenCalledWith('vacancies/');
+   }));
+
+   it('getVacancy with id 1 call test', inject((VacancyService) => {
+      let id = 1;
+      let vacancy = VacancyService.getVacancy(id);
+      expect(mock.get).toHaveBeenCalledWith(`vacancies/${id}`);
+   }));
+
+   it('saveVacancy with id call test', inject((VacancyService) => {
+      let vacancy = {
+         Id: 1
+      };
+
+      VacancyService.saveVacancy(vacancy);
+      expect(mock.put).toHaveBeenCalledWith(`vacancies/${vacancy.Id}`, vacancy);
+   }));
+
+   it('saveVacancy without id call test', inject((VacancyService) => {
+      let vacancy = {
+      };
+
+      VacancyService.saveVacancy(vacancy);
+      expect(mock.post).toHaveBeenCalledWith('vacancies/', vacancy);
+   }));
+
+   it('deleteVacancy', inject((VacancyService) => {
+      let vacancy = {
+         Id: 1
+      };
+
+      VacancyService.deleteVacancy(vacancy);
+      expect(mock.remove).toHaveBeenCalledWith(`vacancies/${vacancy.Id}`, vacancy);
    }));
 });

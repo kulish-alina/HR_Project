5f461fee1be40d83dc204d8cabd51b096e13b182
gasty <Urianna.Rel@gmail.com>
2016-04-18 13:00:37 +0300

    changes for getting datas from backend (updated structure and server address);

--- src/wwwroot/app/services/thesaurusService.js
+++ src/wwwroot/app/services/thesaurusService.js
@@ -7,25 +7,41 @@ const cache = { };
 const THESAURUS_STRUCTURES = {
    'countries' :  {
       thesaurusName : 'THESAURUSES.COUNTRIES',
-      fields : [ {name : 'title', label : 'name', type : 'text'} ]
+      fields : [
+         {name : 'id', label : 'id', type : ''},
+         {name : 'name', label : 'name', type : 'text'},
+         {name : 'editTime', label : 'editTime', type : ''},
+         {name : 'state', label : 'state', type : ''}
+      ]
    },
-   'socials' : {
+   'socialnetworks' : {
       thesaurusName : 'THESAURUSES.SOCIALS',
       fields : [
+         {name : 'id', label : 'id', type : ''},
          {name : 'title', label : 'name', type : 'text' },
-         {name : 'img', label : 'img', type : 'img' }
+         {name : 'imagePath', label : 'image', type : 'img' },
+         {name : 'editTime', label : 'editTime', type : ''},
+         {name : 'state', label : 'state', type : ''}
       ]
    },
    'languages' : {
       thesaurusName : 'THESAURUSES.LANGUAGES',
-      fields : [ {name : 'title', label : 'title', type : 'text' } ]
+      fields : [
+         {name : 'id', label : 'id', type : ''},
+         {name : 'title', label : 'title', type : 'text' },
+         {name : 'editTime', label : 'editTime', type : ''},
+         {name : 'state', label : 'state', type : ''}
+      ]
    },
-   'locations' : {
+   'cities' : {
       thesaurusName : 'THESAURUSES.LOCATIONS',
       fields : [
-         {name : 'title', label : 'name', type : 'text' },
+         {name : 'id', label : 'id', type : ''},
+         {name : 'name', label : 'name', type : 'text' },
          {name : 'country', label : 'country', type : 'select',
-            refTo : 'countries', labelRefFieldName : 'title' , additionFieldForText : 'countryName'}
+           refTo : 'countries', labelRefFieldName : 'title', additionFieldForText : 'countryName'},
+         {name : 'editTime', label : 'editTime', type : ''},
+         {name : 'state', label : 'state', type : ''}
       ]
    }
 };

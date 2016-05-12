56025838ac1dd6ebd6a70aaafefbad7419c53300
SmolenskiyK <ksmo@design.isd.dp.ua>
2016-05-12 15:53:50 +0300

    case

---
+++ src/wwwroot/app/services/CandidateService.js
@@ -0,0 +1,32 @@
+const CANDIDATE_URL = 'candidates/';
+let _HttpService;
+
+export default class CandidateService {
+   constructor(HttpService) {
+      'ngInject';
+      _HttpService = HttpService;
+   }
+
+   getCandidates() {
+      return _HttpService.get(CANDIDATE_URL);
+   }
+
+   getCandidate(id) {
+      let additionalUrl = CANDIDATE_URL + id;
+      return _HttpService.get(additionalUrl);
+   }
+
+   saveCandidate(entity) {
+      if (entity.Id) {
+         let additionalUrl = CANDIDATE_URL + entity.Id;
+         return _HttpService.put(additionalUrl, entity);
+      } else {
+         return _HttpService.post(CANDIDATE_URL, entity);
+      }
+   }
+
+   deleteCandidate(entity) {
+      let additionalUrl = CANDIDATE_URL + entity.Id;
+      _HttpService.remove(additionalUrl, entity);
+   }
+}

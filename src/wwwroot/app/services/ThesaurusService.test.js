e74e39a381a33bf9bf778042be795bb47fb00457
msvaillant <matvienko.serega2012@gmail.com>
2016-05-23 14:58:45 +0300

    Fix: Lint tests now are passing

--- src/wwwroot/app/services/ThesaurusService.test.js
+++ src/wwwroot/app/services/ThesaurusService.test.js
@@ -2,15 +2,7 @@
 
 import ThesaurusService from './ThesaurusService';
 
-import {
-   forEach,
-   has,
-   not
-} from 'lodash';
-
-import THESAURUS_STRUCTURES from './ThesaurusStructuresStore.js';
-
-describe('ThesaurusService testing: ', function() {
+describe('ThesaurusService testing: ', () => {
    let service = null;
    let promiseMock = {
       when:    () => promiseMock,
@@ -29,77 +21,77 @@ describe('ThesaurusService testing: ', function() {
       instant: jasmine.createSpy()
    };
 
-   beforeEach(function() {
+   beforeEach(() => {
       service = new ThesaurusService(mockHttp, promiseMock, mockTranslate);
       mockHttp.get.calls.reset();
       mockHttp.put.calls.reset();
       mockHttp.post.calls.reset();
       mockHttp.remove.calls.reset();
       spyOn(service, 'getThesaurusTopics').and.callThrough();
-   }); 
+   });
 
    function _sendToCacheThesaurus(thesaurusName) {
       spyOn(promiseMock, 'then');
       service.getThesaurusTopics(thesaurusName);
       let callback = promiseMock.then.calls.first().args[0];
       callback({[thesaurusName]: []});
-      promiseMock.then.calls.reset();  
+      promiseMock.then.calls.reset();
    }
-   
-   it('getThesaurusTopics not to be undefined or null', function() {
+
+   it('getThesaurusTopics not to be undefined or null', () => {
       expect(service.getThesaurusTopics).not.toBeUndefined();
       expect(service.getThesaurusTopics).not.toBeNull();
    });
 
-   it('getThesaurusTopic not to be undefined or null', function() {
+   it('getThesaurusTopic not to be undefined or null', () => {
       expect(service.getThesaurusTopic).not.toBeUndefined();
       expect(service.getThesaurusTopic).not.toBeNull();
    });
 
-   it('getThesaurusTopicsGroup not to be undefined or null', function() {
+   it('getThesaurusTopicsGroup not to be undefined or null', () => {
       expect(service.getThesaurusTopicsGroup).not.toBeUndefined();
       expect(service.getThesaurusTopicsGroup).not.toBeNull();
    });
 
-   it('saveThesaurusTopic not to be undefined or null', function() {
+   it('saveThesaurusTopic not to be undefined or null', () => {
       expect(service.saveThesaurusTopic).not.toBeUndefined();
       expect(service.saveThesaurusTopic).not.toBeNull();
    });
 
-   it('saveThesaurusTopics not to be undefined or null', function() {
+   it('saveThesaurusTopics not to be undefined or null', () => {
       expect(service.saveThesaurusTopics).not.toBeUndefined();
       expect(service.saveThesaurusTopics).not.toBeNull();
    });
 
-   it('deleteThesaurusTopic not to be undefined or null', function() {
+   it('deleteThesaurusTopic not to be undefined or null', () => {
       expect(service.deleteThesaurusTopic).not.toBeUndefined();
       expect(service.deleteThesaurusTopic).not.toBeNull();
    });
 
-   it('getThesaurusTopics call test for simple thesaurus', function() {
+   it('getThesaurusTopics call test for simple thesaurus', () => {
       service.getThesaurusTopics('socialnetworks');
       expect(mockHttp.get).toHaveBeenCalledWith('socialnetworks');
    });
 
-   it('getThesaurusTopics call test for complex thesaurus', function() {
-      service.getThesaurusTopics('locations')
+   it('getThesaurusTopics call test for complex thesaurus', () => {
+      service.getThesaurusTopics('locations');
       expect(mockHttp.get).toHaveBeenCalledWith('locations');
       expect(mockHttp.get).toHaveBeenCalledWith('countries');
    });
 
-   it('test cache: http calls once', function() {
+   it('test cache: http calls once', () => {
       _sendToCacheThesaurus('languages');
       service.getThesaurusTopics('languages');
       expect(mockHttp.get.calls.count()).toEqual(1);
    });
 
-   it('getThesaurusTopic with id 1 call test for simple thesaurus not existed to cache', function() {      
+   it('getThesaurusTopic with id 1 call test for simple thesaurus not existed to cache', () => {
       const skillId = 1;
       service.getThesaurusTopic('skills', skillId);
-      expect(service.getThesaurusTopics).toHaveBeenCalledWith(`skills`);
+      expect(service.getThesaurusTopics).toHaveBeenCalledWith('skills');
    });
 
-   it('getThesaurusTopic with id 1 call test for simple thesaurus existed to cache', function() {
+   it('getThesaurusTopic with id 1 call test for simple thesaurus existed to cache', () => {
       const skillId = 1;
       _sendToCacheThesaurus('skills');
       service.getThesaurusTopics.calls.reset();
@@ -121,7 +113,7 @@ describe('ThesaurusService testing: ', function() {
       expect(mockHttp.post).toHaveBeenCalledWith('countries/', country);
    });
 
-   it('saveThesaurusTopic test for only thesaurus saving', function() {
+   it('saveThesaurusTopic test for only thesaurus saving', () => {
       service.saveThesaurusTopic('notThesaurus', {});
       expect(mockHttp.put).not.toHaveBeenCalled();
       expect(mockHttp.post).not.toHaveBeenCalled();
@@ -135,7 +127,7 @@ describe('ThesaurusService testing: ', function() {
       expect(mockHttp.remove).toHaveBeenCalledWith(`countries/${country.id}`, country);
    });
 
-   it('deleteThesaurusTopic test for only thesaurus deletning', function() {
+   it('deleteThesaurusTopic test for only thesaurus deletning', () => {
       service.deleteThesaurusTopic('notThesaurus', {});
       expect(mockHttp.remove).not.toHaveBeenCalled();
    });

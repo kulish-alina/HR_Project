c16e3d56d19b7dfbf09a3a8c082e69afd6882f0b
gasty <Urianna.Rel@gmail.com>
2016-05-25 14:02:37 +0300

    VacancyService unit test template fixes;

--- src/wwwroot/app/services/VacancyService.test.js
+++ src/wwwroot/app/services/VacancyService.test.js
@@ -1,6 +1,6 @@
 'use strict';
 
-import  vacancyService from './VacancyService';
+import  VacancyService from './VacancyService';
 
 describe('VacancyService tests for', () => {
    let service = null;
@@ -12,114 +12,57 @@ describe('VacancyService tests for', () => {
       reject:  () => promiseMock,
       catch:   () => promiseMock
    };
-   let mock = {
+   let mockHttp = {
       get: jasmine.createSpy().and.returnValue(promiseMock),
       post: jasmine.createSpy().and.returnValue(promiseMock),
       put: jasmine.createSpy().and.returnValue(promiseMock),
       remove: jasmine.createSpy().and.returnValue(promiseMock)
    };
    let thesaurusMock = {
-      saveThesaurusTopics: jasmine.createSpy()
+      saveThesaurusTopics: jasmine.createSpy(),
+      getThesaurusTopicsByIds: jasmine.createSpy()
    };
    let loggerMock = {
       error: jasmine.createSpy()
    };
 
+   let userServiceMock = {
+      getUserById: jasmine.createSpy()
+   };
+
    beforeEach(() => {
-      angular.module('test', []).service('VacancyService', vacancyService);
-      angular.mock.module('test');
-      angular.mock.module($provide => {
-         $provide.value('HttpService', mock);
-         $provide.value('ThesaurusService', thesaurusMock);
-         $provide.value('LoggerService', loggerMock);
-         $provide.value('$q', promiseMock);
-      });
+      service = new VacancyService(mockHttp, thesaurusMock, promiseMock, loggerMock, userServiceMock);
    });
 
-   beforeEach(inject(VacancyService => {
-      service = VacancyService;
-   }));
-
-   it('getVacancies not to be undefined or null', () => {
-      expect(service.getVacancies).not.toBeUndefined();
-      expect(service.getVacancies).not.toBeNull();
+   it('search not to be undefined or null', () => {
+      expect(service.search).not.toBeUndefined();
+      expect(service.search).not.toBeNull();
    });
 
-   it('getVacancy not to be null', () => {
-      expect(service.getVacancy).not.toBeUndefined();
-      expect(service.getVacancy).not.toBeNull();
-   });
-
-   it('saveVacancy not to be null', () => {
-      expect(service.saveVacancy).not.toBeUndefined();
-      expect(service.saveVacancy).not.toBeNull();
+   it('save not to be null', () => {
+      expect(service.save).not.toBeUndefined();
+      expect(service.save).not.toBeNull();
    });
 
    it('deleteVacancy not to be null', () => {
-      expect(service.deleteVacancy).not.toBeUndefined();
-      expect(service.deleteVacancy).not.toBeNull();
+      expect(service.remove).not.toBeUndefined();
+      expect(service.remove).not.toBeNull();
    });
 
-   it('getVacancies call test', () => {
-      service.getVacancies();
-      expect(mock.get).toHaveBeenCalledWith('vacancies/');
-   });
-
-   it('getVacancy with id 1 call test', () => {
-      let id = 1;
-      service.getVacancy(id);
-      expect(mock.get).toHaveBeenCalledWith(`vacancies/${id}`);
-   });
-
-   it('saveVacancy with id call test', () => {
-      let vacancy = {
-         id: 1
-      };
-      spyOn(service, '_saveNewTopicsToThesaurus').and.callThrough();
-      spyOn(promiseMock, 'then').and.returnValue(promiseMock);
-      service.saveVacancy(vacancy);
-      let response = {
-         'skills' : [],
-         'tags': []
-      };
-      let callback = promiseMock.then.calls.first().args[0];
-      callback(response);
-      promiseMock.then.calls.reset();
-      expect(mock.put).toHaveBeenCalledWith(`vacancies/${vacancy.id}`, vacancy);
-   });
-
-   it('saveVacancy without id call test', () => {
-      let vacancy = {
-      };
-      spyOn(service, '_saveNewTopicsToThesaurus').and.callThrough();
-      spyOn(promiseMock, 'then').and.returnValue(promiseMock);
-      service.saveVacancy(vacancy);
-      let response = {
-         'skills' : [],
-         'tags': []
-      };
-      let callback = promiseMock.then.calls.first().args[0];
-      callback(response);
-      promiseMock.then.calls.reset();
-
-      service.saveVacancy(vacancy);
-      expect(mock.post).toHaveBeenCalledWith('vacancies/', vacancy);
-   });
-
-   it('deleteVacancy with id', () => {
+   it('remove with id', () => {
       let vacancy = {
          id: 1
       };
 
-      service.deleteVacancy(vacancy);
-      expect(mock.remove).toHaveBeenCalledWith(`vacancies/${vacancy.id}`, vacancy);
+      service.remove(vacancy);
+      expect(mockHttp.remove).toHaveBeenCalledWith(`vacancies/${vacancy.id}`, vacancy);
    });
 
-   it('deleteVacancy without id', () => {
+   it('remove without id', () => {
       let vacancy = {
       };
 
-      expect(service.deleteVacancy).toThrowError(/id/);
-      expect(mock.remove).not.toHaveBeenCalledWith(`vacancies/${vacancy.id}`, vacancy);
+      expect(service.remove).toThrowError(/id/);
+      expect(mockHttp.remove).not.toHaveBeenCalledWith(`vacancies/${vacancy.id}`, vacancy);
    });
 });

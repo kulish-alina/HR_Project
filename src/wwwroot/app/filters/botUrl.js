62e80da958d940e96df8c1c8c3d48d8ad448541d
gasty <Urianna.Rel@gmail.com>
2016-05-25 11:57:32 +0300

    added name for function filter creating by comment to pull request 143

--- src/wwwroot/app/filters/botUrl.js
+++ src/wwwroot/app/filters/botUrl.js
@@ -1,6 +1,6 @@
 import context from './../context';
 
-export default function() {
+export default function urlFilterCreator() {
    return function _botUrl(url) {
       return context.serverUrl + url;
    };

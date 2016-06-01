65dcd8b8263e99f50267562f7de11a44abec00b5
demmy <demiurge.ds@gmail.com>
2016-06-01 11:22:57 +0300

    FileService

--- src/BaseOfTalents/Service/Services/FileService.cs
+++ src/BaseOfTalents/Service/Services/FileService.cs
@@ -23,82 +23,11 @@ namespace Service.Services
             this.repository = repository;
         }
 
-        public FileDTO Add(HttpRequestMessage request, HttpContext currentContext)
+        public FileDTO Add(Domain.Entities.File file)
         {
-            if (!request.Content.IsMimeMultipartContent())
-            {
-                throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
-            }
-            string savingPath = getSavingPath();
-            createDirectoryIfDoesntExists(savingPath);
-            var savedFile = readFileTo(savingPath, request);
-            var domainFile = new Domain.Entities.File()
-            {
-                FilePath = getFilePath(savedFile),
-                Description = getClearFileName(savedFile)
-            };
-            repository.Add(domainFile);
+            repository.Add(file);
             repository.Commit();
-            return DTOService.ToDTO<Domain.Entities.File, FileDTO>(domainFile);
-
+            return DTOService.ToDTO<Domain.Entities.File, FileDTO>(file);
         }
-
-        private MultipartFileData readFileTo(string savingPath, HttpRequestMessage request)
-        {
-            var provider = new BoTMultipartDataStreamProvider(savingPath);
-            request.Content.ReadAsMultipartAsync(provider).ContinueWith((data) =>
-            {
-                if (data.IsFaulted)
-                {
-                    throw data.Exception;
-                }
-            });
-            return provider.FileData[0];
-        }
-
-        private void createDirectoryIfDoesntExists(string savingPath)
-        {
-            if (!Directory.Exists(savingPath))
-            {
-                Directory.CreateDirectory(savingPath);
-            }
-        }
-
-        private string getClearFileName(MultipartFileData savedFile)
-        {
-            return savedFile.Headers.ContentDisposition.FileName.Replace("\"", string.Empty);
-        }
-
-        private string getFilePath(MultipartFileData savedFile)
-        {
-            string serverPath = HttpContext.Current.Server.MapPath("~");
-            var removedServerPath = savedFile.LocalFileName.Replace(serverPath, string.Empty);
-            var replacedWrongSlashes = removedServerPath.Replace(@"\", @"/");
-            return replacedWrongSlashes;
-        }
-
-        private string getSavingPath()
-        {
-            string rootPath = HttpContext.Current.Server.MapPath("~/Upload");
-            string thisYearPath = rootPath + @"/" + DateTime.Now.Year;
-            string finalPath = thisYearPath + @"/" + DateTime.Now.GetIso8601WeekOfYear();
-            return finalPath;
-        }
-    }
-
-    class BoTMultipartDataStreamProvider : MultipartFormDataStreamProvider
-    {
-        public BoTMultipartDataStreamProvider(string path) : base(path)
-        {
-
-        }
-
-        public override string GetLocalFileName(HttpContentHeaders headers)
-        {
-            int uniqueFileIdentifier = Directory.GetFiles(RootPath).Length;
-            string fileName = (uniqueFileIdentifier + headers.ContentDisposition.FileName).Replace("\"", string.Empty);
-            return fileName;
-        }
-
     }
 }

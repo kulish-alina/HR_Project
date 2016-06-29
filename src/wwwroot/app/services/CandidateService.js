b084d6b4bc5ef20715f0e5324e79a7501d959a9c
gasty <Urianna.Rel@gmail.com>
2016-06-29 22:52:04 +0300

    fixes after merging;
    coming soon added;
    
    vain source saving added;

--- src/wwwroot/app/services/CandidateService.js
+++ src/wwwroot/app/services/CandidateService.js
@@ -29,17 +29,14 @@ const THESAURUSES = [
    new ThesaurusHelper('tag',   'tagIds',   'tags',     true),
    new ThesaurusHelper('skill', 'skillIds', 'skills',   true),
 
-   new ThesaurusHelper('industry',          'industryId',        'industry',         true),
-   new ThesaurusHelper('city',              'cityId',            'city',             true),
-   new ThesaurusHelper('typeOfEmployment',  'typeOfEmployment',  'typeOfEmployment', true),
-   new ThesaurusHelper('level',             'levelId',           'level',            true),
-   new ThesaurusHelper('currency',          'currencyId',        'currency',         true),
-   new ThesaurusHelper('socialNetwork',     'socialNetworks',    'socials',          true),
-   new ThesaurusHelper('state',             'stateId',           'state',            true),
-   new ThesaurusHelper('source',            'sourceId',          'source',           true)
+   new ThesaurusHelper('industry',          'industryId',        'industry'),
+   new ThesaurusHelper('city',              'cityId',            'city'),
+   new ThesaurusHelper('typeOfEmployment',  'typeOfEmployment',  'typeOfEmployment'),
+   new ThesaurusHelper('level',             'levelId',           'level'),
+   new ThesaurusHelper('currency',          'currencyId',        'currency'),
+   new ThesaurusHelper('socialNetwork',     'socialNetworks',    'socials'),
+   new ThesaurusHelper('source',            'mainSourceId',      'source')
 ];
-
-let _HttpService, _ThesaurusService, _$q;
 
 let _HttpService, _ThesaurusService, _$q;
 
@@ -84,134 +81,7 @@ export default class CandidateService {
             return response;
          });
       });
-      
-    
-function _addSavedThesaurusesTopics(candidate, thesaurusName, topics) {
-   candidate[thesaurusName] = union(candidate[thesaurusName] || [],  topics);
-   return candidate;
-}
-
-function _saveNewThesaurusTopics(candidate) {    
-   let promises = reduce(THESAURUSES, (memo, helper) => {
-      let newTopicPattern = {id : undefined};
-      let topicsToSave = filter(candidate[helper.clientField], newTopicPattern);
-      if (topicsToSave.length) {
-         remove(candidate[helper.clientField], newTopicPattern);
-         memo[helper.clientField] = _ThesaurusService.saveThesaurusTopics(helper.thesaurusName, topicsToSave)
-            .then(_addSavedThesaurusesTopicsCurried(candidate, helper.thesaurusName));
-      }
-      return memo;
-   }, {});
-   return _$q.all(promises);
-}
-
-function  _convertThesaurusToIds(candidate) {
-   forEach(THESAURUSES, helper => {
-      if (helper.needConvertForServer) {
-         candidate[helper.serverField] = map(candidate[helper.clientField], 'id');
-      }
-   });
-   return candidate;
-}
-
-function _convertToClientFormat(candidate) {
-   _convertFromServerDates(candidate);
-   _addReferencedThesaurusObjects(candidate);
-   _setMonthYearExperience(candidate);
-   _convertRelocationPlacesToClient(candidate);
-   _convertLanguageSkillsToClient(candidate);
-   _convertSocialToClient(candidate);
-   return candidate;
-}
-
-function _convertToServerFormat(candidate) {
-   _convertToServerDates(candidate);
-   _convertThesaurusToIds(candidate);
-   _deleteReferencedThesaurusObjects(candidate);
-   _setStartExperience(candidate);
-   _convertRelocationPlacesToBackend(candidate);
-   _convertLanguageSkillsToBackend(candidate);
-   _convertSocialToBackend(candidate);
-   delete candidate.experienceYears;
-   delete candidate.experienceMonthes;
-   return candidate;
-}
-
-function _convertSocialToClient(candidate) {
-   let convertedSocials = map(candidate.socialNetworks, social => _getCandidateSocialObject(social));
-   candidate.convertedSocials = convertedSocials;
-   return candidate;
-}
-
-function _convertSocialToBackend(candidate) {
-   let changedSocials = unionWith(
-      candidate.socialNetworks || [],
-      map(candidate.convertedSocials, _convertSocialFieldsToBackend),
-      isEqualSocials);
-
-   let deletedSocials = differenceWith(
-      changedSocials,
-      candidate.convertedSocials,
-      isEqualSocials);
-
-   forEach(deletedSocials, social => social.state = DELETED_STATE);
-   candidate.socialNetworks = changedSocials;
-   delete candidate.convertedSocials;
-   return candidate;
-}
-
-function _convertLanguageSkillsToBackend(candidate) {
-   let changedLanguageSkills = unionWith(
-      candidate.languageSkills,
-      map(candidate.convertedLanguageSkills, _convertLanguageSkillFieldsToBackend),
-      isEqualLanguageSkills);
-
-   let deletedLanguageSkills = differenceWith(
-      changedLanguageSkills,
-      candidate.convertedLanguageSkills,
-      isEqualLanguageSkills);
-
-   forEach(deletedLanguageSkills, skill => skill.state = DELETED_STATE);
-   candidate.languageSkills = changedLanguageSkills;
-   delete candidate.convertedLanguageSkills;
-   return candidate;
-}
-
-function isEqualLanguageSkills(languageSkill1, languageSkill2) {
-   return languageSkill1.languageId === languageSkill2.languageId &&
-      languageSkill1.languageLevel === languageSkill2.languageLevel;
-}
-
-function isEqualRelocationPlaces(relocationPlace1, relocationPlace2) {
-   return relocationPlace1.countryId === relocationPlace2.countryId &&
-      relocationPlace1.cityId === relocationPlace2.cityId;
-}
-
-function isEqualSocials(social1, social2) {
-   return social1.socialNetworkId === social2.socialNetworkId;
-}
-
-function _getLanguageSkillObgect(sourceLanguageSkill) {
-   let convertedLanguageSkill = {};
-   _ThesaurusService.getThesaurusTopic('language', sourceLanguageSkill.languageId)
-      .then(curriedSet(convertedLanguageSkill, 'language'));
-   _ThesaurusService.getThesaurusTopic('languageLevel', sourceLanguageSkill.languageLevel)
-      .then(level => {
-         convertedLanguageSkill.languageLevel = isNull(level) ? undefined : level;
-      });
-   return convertedLanguageSkill;
-}
-
-function _getRelocationPlaceObgect(sourceRelocationPlace) {
-   let convertedRelocationPlace = {};
-   _ThesaurusService.getThesaurusTopic('country', sourceRelocationPlace.countryId)
-      .then(curriedSet(convertedRelocationPlace, 'country'));
-   _ThesaurusService.getThesaurusTopic('city', sourceRelocationPlace.cityId)
-      .then(city => {
-         convertedRelocationPlace.city = isNull(city) ? undefined : city;
-      });
-   return convertedRelocationPlace;
-}
+   }
 }
 
 function _addSavedThesaurusesTopics(candidate, thesaurusName, topics) {
@@ -401,19 +271,15 @@ function _convertRelocationsFieldsToBackend(place) {
 }
 
 function _addReferencedThesaurusObjects(candidate) {
-   forEach(THESAURUSES, ({thesaurusName, clientField, serverField, needConvertForServer}) => {
-      if (needConvertForServer) {
-         _ThesaurusService.getThesaurusTopicsByIds(thesaurusName, candidate[serverField])
-            .then(curriedSet(candidate, clientField));
-      }
+   forEach(THESAURUSES, ({thesaurusName, clientField, serverField}) => {
+      _ThesaurusService.getThesaurusTopicsByIds(thesaurusName, candidate[serverField])
+         .then(curriedSet(candidate, clientField));
    });
 }
 
 function _deleteReferencedThesaurusObjects(candidate) {
    forEach(THESAURUSES, refThesaurus => {
-      if (refThesaurus.needConvertForServer) {
-         delete candidate[refThesaurus.clientField];
-      }
+      delete candidate[refThesaurus.clientField];
    });
 }
 

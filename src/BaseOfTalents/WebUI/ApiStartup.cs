9056c89b054c27e2d9567957877226e0bd3b6425
msvaillant <matvienko.serega2012@gmail.com>
2016-07-18 19:15:46 +0300

    Feature: web page response and program configuration

---
+++ src/BaseOfTalents/WebUI/ApiStartup.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Http;
+using Microsoft.Owin;
+using Microsoft.Owin.FileSystems;
+using Microsoft.Owin.StaticFiles;
+using Owin;
+using WebUI.App_Start;
+
+[assembly: OwinStartup(typeof(BaseOfTalents.WebUI.ApiStartup))]
+
+namespace BaseOfTalents.WebUI
+{
+    public class ApiStartup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            app.UseWebApi(config);
+
+            const string rootFolder = ".";
+            var fileSystem = new PhysicalFileSystem(rootFolder);
+            var options = new FileServerOptions
+            {
+                EnableDefaultFiles = true,
+                FileSystem = fileSystem
+            };
+
+            app.UseFileServer(options);
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+        }
+    }
+}
---
+++ src/BaseOfTalents/WebUI/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net.Http;
+using Microsoft.Owin.Hosting;
+
+namespace BaseOfTalents.WebUI
+{
+    public class Program
+    {
+
+        static void Main()
+        {
+#if !RELEASE
+            int port = 8080;
+            string options = $"http://localhost:{port}/";
+
+#else
+            int port = 9000;
+            StartOptions options = new StartOptions($"http://+:{port}")
+            {
+                ServerFactory = "Microsoft.Owin.Host.HttpListener"
+            };
+
+            //StartOptions options = new StartOptions();
+            //options.Urls.Add($"http://localhost:{port}");
+            //options.Urls.Add($"http://127.0.0.1:{port}");
+            //options.Urls.Add($"http://192.168.0.102:{port}");
+            //options.Urls.Add($"http://{Environment.MachineName}:{port}");
+
+#endif
+            using (WebApp.Start<ApiStartup>(options))
+            {
+                Console.WriteLine("Server started...");
+                Console.WriteLine($"Machine name: {Environment.MachineName}");
+#if DEBUG
+                HttpClient client = new HttpClient();
+                var res = client.GetAsync($"http://localhost:{port}/api/tag").Result;
+                Console.WriteLine($"{res.Content.ReadAsStringAsync().Result}");
+#endif
+                Console.WriteLine("Press ENTER to stop server");
+                Console.ReadLine();
+            }
+            Console.WriteLine("End");
+        }
+    }
+}

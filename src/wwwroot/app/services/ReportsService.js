c2797296c49c6777e285eb5afca6da16f387b0d8
kulish-alina <kylish.alina.dp@gmail.com>
2016-09-05 19:51:03 +0300

    users.report.controller was created
    ReportsService was created

--- src/wwwroot/app/services/ReportsService.js
+++ src/wwwroot/app/services/ReportsService.js
@@ -1,6 +1,29 @@
+import utils  from '../utils.js';
+import {
+   each
+} from 'lodash';
+const DATE_TYPE = ['startDate', 'endDate'];
+const REPORT_URL = 'report';
+let _HttpService;
+
 export default class ReportsService {
-   constructor() {
+   constructor(HttpService) {
       'ngInject';
+      _HttpService   = HttpService;
    }
 
+   getDataForUserReport(searchParameters) {
+      searchParameters = this._convertToServerDates(searchParameters);
+      let userReportUrl = 'usersReport';
+      return _HttpService.post(`${REPORT_URL}/${userReportUrl}`, searchParameters);
+   }
+
+   _convertToServerDates(searchParameters) {
+      each(DATE_TYPE, (type) => {
+         if (searchParameters[type]) {
+            searchParameters[type] = utils.formatDateToServer(searchParameters[type]);
+         };
+      });
+      return searchParameters;
+   }
 }
--- src/wwwroot/app/views/reports/users-report/users.report.controller.js
+++ src/wwwroot/app/views/reports/users-report/users.report.controller.js
@@ -1,19 +1,116 @@
+import './user.report.scss';
 import {
-   set
+   set,
+   remove,
+   assign,
+   each
 } from 'lodash';
+const LIST_OF_LOCATIONS = ['Dnipropetrovsk', 'Zaporizhia', 'Lviv', 'Berdyansk'];
 
 export default function UsersReportController(
    $scope,
-   UserService
+   UserService,
+   ThesaurusService,
+   ReportsService
 ) {
    'ngInject';
 
-   const vm                   = $scope;
-   vm.users                   = [];
-   vm.listOfLocations         = ['Dnipro', 'Zaporіzhzhya', 'Lviv', 'Berdyansk'];
+   const vm                                = $scope;
+   vm.users                                = [];
+   vm.usersReportParametrs                 = {};
+   vm.usersReportParametrs.locationIds     = [];
+   vm.usersReportParametrs.userIds         = [];
+   vm.locations                            = [];
+   vm.selectedLocations                    = [];
+   vm.selectedUsers                        = [];
+   vm.selectedUsersGroupedByLocation       = {};
+   vm.clear                                = clear;
+   vm.addLocationIdsToUsersReportParametrs = addLocationIdsToUsersReportParametrs;
+   vm.addUserIdsToUsersReportParametrs     = addUserIdsToUsersReportParametrs;
+   vm.formingUsersReport                   = formingUsersReport;
+   vm.isEqualLocations                     = isEqualLocations;
 
    (function init() {
-      UserService.getUsers().then(users => set(vm, 'users', users));
+      ThesaurusService.getThesaurusTopics('stage').then(topic => set(vm, 'stages', topic));
+      ThesaurusService.getThesaurusTopics('city').then(locations => {
+         each(locations, (location) => {
+            if (LIST_OF_LOCATIONS.includes(location.title)) {
+               vm.locations.push(location);
+            }
+            if (vm.usersReportParametrs.locationIds.length &&
+                vm.usersReportParametrs.locationIds.includes(location.id)) {
+               assign(location, {selected: true});
+            } else {
+               assign(location, {selected: false});
+            }
+         });
+      });
+      UserService.getUsers().then(users => {
+         set(vm, 'users', users);
+         each(vm.users, (user) => {
+            if (vm.usersReportParametrs.userIds.length &&
+                vm.usersReportParametrs.userIds.includes(user.id)) {
+               assign(user, {selected: true});
+            } else {
+               assign(user, {selected: false});
+            }
+         });
+      });
    }());
+
+   function addLocationIdsToUsersReportParametrs(location) {
+      if (location.selected && !vm.usersReportParametrs.locationIds.includes(location.id)) {
+         vm.usersReportParametrs.locationIds.push(location.id);
+         vm.selectedLocations.push(location);
+      } else if (!location.selected) {
+         remove(vm.usersReportParametrs.locationIds, (locationId) =>  locationId === location.id);
+         remove(vm.selectedLocations, (loc) =>  loc.id === location.id);
+      }
+   }
+
+   function addUserIdsToUsersReportParametrs(user) {
+      if (user.selected && !vm.usersReportParametrs.userIds.includes(user.id)) {
+         vm.usersReportParametrs.userIds.push(user.id);
+         if (vm.selectedLocations.length) {
+            _convertUsersArrayToHash(user);
+         } else {
+            vm.selectedUsers.push(user);
+         }
+      } else if (!user.selected) {
+         remove(vm.usersReportParametrs.userIds, (userId) =>  userId === user.id);
+         if (vm.selectedLocations.length) {
+            remove(vm.selectedUsersGroupedByLocation[user.cityId], {id: user.id});
+         } else {
+            remove(vm.selectedUsers, (us) =>  us.id === user.id);
+         }
+      }
+   }
+
+   function formingUsersReport() {
+      ReportsService.getDataForUserReport(vm.usersReportParametrs);
+   }
+
+   function clear() {
+      vm.usersReportParametrs = {};
+   }
+
+   function isEqualLocations(user) {
+      if (vm.usersReportParametrs.locationIds.length) {
+         return vm.usersReportParametrs.locationIds.includes(user.cityId);
+      } else {
+         return true;
+      }
+   }
+
+   function _convertUsersArrayToHash(user) {
+      each(vm.selectedLocations, location => {
+         if (vm.selectedUsersGroupedByLocation[location.id] && location.id === user.cityId) {
+            vm.selectedUsersGroupedByLocation[location.id].push(user);
+         } else if (!vm.selectedUsers[location.id] && location.id === user.cityId) {
+            vm.selectedUsersGroupedByLocation[location.id] = [];
+            vm.selectedUsersGroupedByLocation[location.id].push(user);
+         }
+      });
+   }
 
 }

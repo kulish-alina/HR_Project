d9c64efed5595e813e5dae8154b0694b93ff98d3
kulish-alina <kylish.alina.dp@gmail.com>
2016-09-08 17:05:39 +0300

    method addStageFilter was added to recruiting.funnel.controller

--- src/wwwroot/app/views/reports/recruiting-funnel/recruiting.funnel.controller.js
+++ src/wwwroot/app/views/reports/recruiting-funnel/recruiting.funnel.controller.js
@@ -1,6 +1,8 @@
 import {
    set,
-   each
+   each,
+   assign,
+   remove
 } from 'lodash';
 
 export default function RecruitingFunnelController(
@@ -13,18 +15,28 @@ export default function RecruitingFunnelController(
    const vm                                       = $scope;
    vm.formingRecruitingFunnelReport               = formingRecruitingFunnelReport;
    vm.viewVacancy                                 = viewVacancy;
+   vm.viewCandidate                               = viewCandidate;
    vm.clear                                       = clear;
    vm.candidatesGropedByStage                     = {};
+   vm.selectVacancy                               = selectVacancy;
+   vm.addStageFilter                              = addStageFilter;
+   vm.selectedStageIds                            = [];
 
    (function init() {
-      ThesaurusService.getThesaurusTopics('stage').then(topic => set(vm, 'stages', topic));
+      ThesaurusService.getThesaurusTopics('stage').then(topic => {
+         set(vm, 'stages', topic);
+         each(vm.stages, (stage) => {
+            assign(stage, {_isPressed: true});
+            vm.selectedStageIds.push(stage.id);
+         });
+      });
    }());
 
    function formingRecruitingFunnelReport() {
-      _formingDataForRecruitingFunnelReport();
    }
 
-   function _formingDataForRecruitingFunnelReport() {
+   function selectVacancy() {
+      vm.candidatesGropedByStage = {};
       each(vm.selectedVacancy.candidatesProgress, (candidateStage) => {
          if (vm.candidatesGropedByStage[candidateStage.stageId]) {
             vm.candidatesGropedByStage[candidateStage.stageId].push(candidateStage.candidate);
@@ -35,11 +47,30 @@ export default function RecruitingFunnelController(
       });
    }
 
+   function addStageFilter(stage) {
+      if (stage._isPressed) {
+         stage._isPressed = false;
+         remove(vm.selectedStageIds, (stageId) =>  stageId === stage.id);
+      } else {
+         stage._isPressed = true;
+         vm.selectedStageIds.push(stage.id);
+      }
+   }
+
    function clear() {
       vm.selectedVacancy = {};
+      vm.selectedStageIds = [];
+      each(vm.stages, (stage) => {
+         stage._isPressed = false;
+      });
+      vm.candidatesGropedByStage = {};
    }
 
    function viewVacancy() {
       $state.go('vacancyView', {_data: null, vacancyId: vm.selectedVacancy.id});
+   }
+
+   function viewCandidate(selectedCandidateId) {
+      $state.go('candidateProfile', {_data: null, candidateId: selectedCandidateId});
    }
 }

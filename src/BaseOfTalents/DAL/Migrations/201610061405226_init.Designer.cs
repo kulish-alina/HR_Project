a64aa83058d55d118dcb80888c3aebdc484c7dcb
msvaillant <matvienko.serega2012@gmail.com>
2016-09-29 16:16:38 +0300

    Fix: Remove useless migrations!

--- src/BaseOfTalents/DAL/Migrations/201610061405226_init.Designer.cs
+++ src/BaseOfTalents/DAL/Migrations/201610061405226_init.Designer.cs
@@ -13,7 +13,7 @@ namespace DAL.Migrations
         
         string IMigrationMetadata.Id
         {
-            get { return "201609160956375_init"; }
+            get { return "201610061405226_init"; }
         }
         
         string IMigrationMetadata.Source
